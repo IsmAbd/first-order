85ef7381235f9c24a01f3d9e383d3397643f8096
Möller Yannick <ym018@hdm-stuttgart.de>
2019-05-22 15:55:13 +0200

    updated RootQuerys

--- server/graphql/resolvers/resolverGenerator/productResolver.js
+++ server/graphql/resolvers/resolverGenerator/productResolver.js
@@ -8,7 +8,7 @@ export async function addProduct(args) {
         description: description,
         category: category,
         type: type,
-        image_path:image_path
+        image_path: image_path
     };
 
     var product = new Product(tempProduct);
