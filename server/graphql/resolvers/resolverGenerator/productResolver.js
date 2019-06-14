4b57cfa5fa2df352342b490b96c43d931deb62b4
Möller Yannick <ym018@hdm-stuttgart.de>
2019-06-14 14:14:33 +0200

    updated productResolver

--- server/graphql/resolvers/resolverGenerator/productResolver.js
+++ server/graphql/resolvers/resolverGenerator/productResolver.js
@@ -3,46 +3,46 @@ import Restaurant from "../../../models/restaurant";
 import Category from "../../../models/category";
 
 export async function addProduct(args) {
-  const { name, description, categoryID, type, image_path } = args.userInput;
+    const {name, description, categoryID, type, image_path} = args.userInput;
 
-  const tempProduct = {
-    name: name,
-    description: description,
-    type: type,
-    image_path: image_path
-  };
+    const tempProduct = {
+        name: name,
+        description: description,
+        type: type,
+        image_path: image_path
+    };
 
-  let product = new Product(tempProduct);
+    let product = new Product(tempProduct);
 
-  let category = await Category.filter({ id: categoryID }).then(result => {
-    return result[0];
-  });
+    let category = await Category.filter({id: categoryID}).then(result => {
+        return result[0];
+    });
 
-  category.products = product;
-  product.category = category;
+    category.products = product;
+    product.category = category;
 
-  let result = await product.saveAll({ category: true }).then(result => {
+    let result = await product.saveAll({category: true}).then(result => {
+        return result;
+    });
+
     return result;
-  });
-
-  return result;
 }
 
 export async function getProductByID(args) {
-  let result = await Product.filter({ id: args.id })
-    .getJoin({ category: true })
-    .then(result => {
-      console.log(result[0]);
-      return result[0];
-    });
+    let result = await Product.filter({id: args.id})
+        .getJoin({category: true})
+        .then(result => {
+            console.log(result[0]);
+            return result[0];
+        });
 
-  return result;
+    return result;
 }
 
 export async function getAllProductsByRestaurant() {
-  let result = await Restaurant.getJoin({ category: true }).then(result => {
-    return result;
-  });
+    let result = await Restaurant.getJoin({category: true, product: {price: true}}).then(result => {
+        return result;
+    });
 
-  return result;
+    return result;
 }
